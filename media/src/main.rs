6955f3cfdde4d31e6b805f64662d28c489d0f82a
zubayr1 <write2zubayr@gmail.com>
2025-03-02 00:00:52 +0100

    add impl

--- media/src/main.rs
+++ media/src/main.rs
@@ -1,3 +1,42 @@
+#[derive(Debug)]
+enum Media {
+    Book{
+        title: String,
+        author: String,
+    },
+    Movie{
+        title: String,
+        director: String,
+    },
+    AudioBook{
+        title: String,
+    },
+}
+
+impl Media {
+    fn description(&self) -> String {
+        match self {
+            Media::Book{title, author} => format!("Book: {} by {}", title, author),
+            Media::Movie{title, director} => format!("Movie: {} by {}", title, director),
+            Media::AudioBook{title} => format!("AudioBook: {}", title),
+        }
+    }
+}
+
+fn print_media(media: &Media) {
+    match media {
+        Media::Book{title, author} => println!("Book: {} by {}", title, author),
+        Media::Movie{title, director} => println!("Movie: {} by {}", title, director),
+        Media::AudioBook{title} => println!("AudioBook: {}", title),
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let book = Media::Book{
+        title: "The Hobbit".to_string(),
+        author: "J.R.R. Tolkien".to_string(),
+    };
+
+    print_media(&book);
 }
